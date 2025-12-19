e3ac69b26053c004f7aa10476bdeaf4ec4c9d7d1
heikki pesonen <heikki.pesonen@sok.fi>
2025-12-19 09:28:51 +0200

    feat(git): commit with different branch names

--- do-git-branch.sh
+++ do-git-branch.sh
@@ -3,11 +3,18 @@
 user_name=$(git config user.name)
 initials=$(echo $user_name | awk '{print tolower(substr($1,1,1)) tolower(substr($2,1,1))}')
 
-if [ "$#" -ne 2 ]; then
-  echo "Usage: do-git-branch.sh <JIRA-TICKET> <DESCRIPTION>"
+if [ "$#" -eq 1 ]; then
+  # Branch without ticket number
+  echo "Creating branch $initials/$1"
+  git checkout -b $initials/$1
+elif [ "$#" -eq 2 ]; then
+  # Branch with ticket number
+  echo "Creating branch $initials/$1/$2"
+  git checkout -b $initials/$1/$2
+else
+  echo "Usage: do-git-branch.sh <DESCRIPTION>"
+  echo "   or: do-git-branch.sh <JIRA-TICKET> <DESCRIPTION>"
   exit 1
 fi
-echo "Creating branch $initials/$1/$2"
-git checkout -b $initials/$1/$2
 exit 0
 
--- do-git-commit.sh
+++ do-git-commit.sh
@@ -4,6 +4,31 @@ if [ "$#" -ne 3 ]; then
   echo "Usage: do-git-commit.sh c <type> <subject> <description>"
   exit 1
 fi
+
 # get ticket number from branch name
-ticketNum=$(git rev-parse --abbrev-ref HEAD  | cut -d '/' -f2)
-git commit -m "[$ticketNum]$1($2): $3" --no-verify
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+# Try to extract ticket number from branch
+# Assumes format like initials/TICKET-123/desc or TICKET-123/desc or feature/TICKET-123
+if [[ "$branch" == *"/"* ]]; then
+  # Branch has slashes, try to find ticket in second or first segment
+  first_segment=$(echo "$branch" | cut -d '/' -f1)
+  second_segment=$(echo "$branch" | cut -d '/' -f2)
+  
+  if [[ "$second_segment" =~ ^[A-Z]+-[0-9]+$ ]]; then
+    ticketNum="$second_segment"
+  elif [[ "$first_segment" =~ ^[A-Z]+-[0-9]+$ ]]; then
+    ticketNum="$first_segment"
+  else
+    ticketNum=""
+  fi
+else
+  ticketNum=""
+fi
+
+# Check if we found a valid ticket number
+if [[ -n "$ticketNum" ]]; then
+  git commit -m "[$ticketNum]$1($2): $3" --no-verify
+else
+  git commit -m "$1($2): $3" --no-verify
+fi
